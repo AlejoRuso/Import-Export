31dc9089950055eba7c4fcd54f71f9954441ff04
Alejo <istominalexey@ro.ru>
2025-06-16 13:50:21 +0300

    config fixed

--- src/game.js
+++ src/game.js
@@ -1,18 +1,14 @@
-// Импорт Character из domain.js
 import Character from './domain.js';
 
-// Экспорт Game как default
 export default class Game {
   start() {
     console.log('game started');
   }
 }
 
-// Экспорт GameSavingData
 export class GameSavingData {
 }
 
-// Экспорт функций
 export function readGameSaving() {
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Game Application',
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+              "modules": "commonjs"
+            }]]
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development',
+  stats: {
+    errorDetails: true
+  }
+};
